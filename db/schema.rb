6eed09d360cd6d913a51d2573608395aabf229b9
sugi <inet.sugiyama@gmail.com>
2015-03-02 07:19:38 +0900

    migrate_file_change

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150301203754) do
+ActiveRecord::Schema.define(version: 20150301221700) do
 
   create_table "microposts", force: true do |t|
     t.string   "content"
